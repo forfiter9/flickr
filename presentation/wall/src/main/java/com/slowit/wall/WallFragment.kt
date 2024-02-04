86f79a71fabbee09a09f0d29f2c31f25d3940a1a
Tomasz Kaiser <t.kaiser@bsgroup.eu>
2024-02-04 23:11:07 +0100

    on item click open browser

--- presentation/wall/src/main/java/com/slowit/wall/WallFragment.kt
+++ presentation/wall/src/main/java/com/slowit/wall/WallFragment.kt
@@ -1,15 +1,17 @@
 package com.slowit.wall
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.slowit.wall.databinding.FragmentWallBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class WallFragment : Fragment() {
@@ -38,7 +40,9 @@ class WallFragment : Fragment() {
             layoutManager = if (context.isTablet()) GridLayoutManager(context, SPAN_COUNT_TABLET) else GridLayoutManager(context, SPAN_COUNT_PHONE)
             adapter = WallAdapter().apply {
                 onPostClickListener = WallAdapter.OnPostClickListener {
-                    Toast.makeText(context,it.title,Toast.LENGTH_SHORT).show()
+                    val browserIntent =
+                        Intent(Intent.ACTION_VIEW, Uri.parse(it.link))
+                    startActivity(browserIntent)
                 }
             }
         }
